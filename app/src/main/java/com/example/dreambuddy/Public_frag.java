0112f040bfc2ddae3677606d992237f16fee1479
Evan Mays <me@evanmays.com>
2019-04-03 20:06:27 -0400

    Added recycler view to public post page

--- app/src/main/java/com/example/dreambuddy/Public_frag.java
+++ app/src/main/java/com/example/dreambuddy/Public_frag.java
@@ -4,15 +4,36 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Public_frag extends Fragment{
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.public_tab, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.public_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        String[] myDataset = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
         return view;
     }
 }
