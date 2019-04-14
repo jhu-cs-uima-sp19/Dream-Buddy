cbcd817da80fcbaf42c6f594b98ea851945ace36
Nico <nivanov4@jhu.com>
2019-04-14 17:29:59 -0400

    Updated post view and added exit nav. Also added lines to the post_item

--- app/src/main/java/com/example/dreambuddy/ViewPost.java
+++ app/src/main/java/com/example/dreambuddy/ViewPost.java
@@ -1,12 +1,32 @@
 package com.example.dreambuddy;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ViewPost extends AppCompatActivity {
     JournalEntry post;
     TextView postTitle;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.exit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_exit) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +35,9 @@ public class ViewPost extends AppCompatActivity {
 
         this.post = (JournalEntry) getIntent().getSerializableExtra("journalEntry");
 
-        postTitle = this.findViewById(R.id.Post_Title);
-        postTitle.setText(post.getTitle());
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        TextView title = findViewById(getResources().getIdentifier("action_bar_title", "id", getPackageName()));
+        title.setText(R.string.title_view_post);
     }
 }
